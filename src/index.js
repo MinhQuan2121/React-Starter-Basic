ee355cf41cfd3be156103cb58e00a52fcf9f33a8
Minhquan2121 <qdang5474@gmail.com>
2024-10-09 17:57:25 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  return (
+    <div>
+      <h1>Xin chào anh em F8!</h1>
+      <h1>Xin chào anh em F8!</h1>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
